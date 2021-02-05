7cb1db5345402434865b6da4941ef2dfd35e96a4
Max J. Webber <tua83483@temple.edu>
2021-02-05 12:05:33 -0500

    Initial commit

---
+++ generateTestData.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+use 5.32.1;
+use warnings FATAL => 'all';
+use List::Util qw(shuffle);
+
+#0 is not infected, 1 is infected
+
+#write 100-1000 lines to one file
+#command line params: how many infected, size of the pool, how many lines of test data
+
+my ($num_infected, $num_testpool, $num_lines) = @ARGV;
+my @testpool;
+my @countA = (1..$num_infected);
+for (@countA){
+    #say("$_");
+    push (@testpool,1);
+}
+my @countB = (($num_infected+1)..$num_testpool);
+for (@countB){
+    #say("$_");
+    push (@testpool,0);
+}
+
+my $filename = "testdata.txt";
+
+open(FH, '>', $filename) or die $!;
+
+for (1..$num_lines){
+    @testpool = shuffle(@testpool);
+    #say("$_");
+    say FH @testpool;
+}
+close(FH);
