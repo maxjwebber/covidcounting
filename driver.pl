361bd65d035fc57f6224d7d8eb0466f98aeab0a3
Max J. Webber <tua83483@temple.edu>
2021-02-17 18:03:10 -0500

    replaced Benchmark with Time::HiRes (more accurate.)

--- driver.pl
+++ driver.pl
@@ -5,30 +5,29 @@ require "ACAI.pl";
 require "ACAP.pl";
 require "generateTestData.pl";
 use Text::CSV qw(csv);
-use Benchmark;
-my $t0 = Benchmark->new;
+use Time::HiRes qw(gettimeofday tv_interval);
+my $t0 = [gettimeofday];
 
-my @k = (1..205);
-my $n = 1023;
-my $trialsPerK = 10000;
+my @k = (205);
+my $n = 1000000;
+my $trialsPerK = 5;
 my @ACAresults;
 my @headers = ("k","Sample Mean (Y)","Sample Variance (Y)","Sample Mean (2^Y)","Sample Variance (2^Y)");
 my $thisk;
 my $csv = Text::CSV->new ();
-open my $fh, ">:encoding(utf8)", "results.csv" or die "results.csv: $!";
-$csv->bind_columns (\(@headers));
-$csv->say ($fh,\@headers);
+#open my $fh, ">:encoding(utf8)", "results.csv" or die "results.csv: $!";
+#$csv->bind_columns (\(@headers));
+#$csv->say ($fh,\@headers);
 for (@k)
 {
     $thisk = $_;
     generateTestData($thisk,$n,$trialsPerK);
-    @ACAresults = ACAI();
+    @ACAresults = ACAP();
     unshift(@ACAresults,$thisk);
     #say("@ACAresults");
     $csv->say ($fh,\@ACAresults);
 }
 close($fh);
 
-my $t1 = Benchmark->new;
-my $td = timediff($t1, $t0);
-print "the process took ",timestr($td);
+my $elapsed = tv_interval ($t0);
+print "the whole process took $elapsed seconds.";
